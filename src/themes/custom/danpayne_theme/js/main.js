6e8bb318cef766c8f4f808787a6b8b4b713dab12
Philippe Joulot <philippe.joulot@laposte.net>
2016-12-11 20:16:30 +0100

    Adapt the menu on mobile devices

--- src/themes/custom/danpayne_theme/js/main.js
+++ src/themes/custom/danpayne_theme/js/main.js
@@ -8,16 +8,23 @@
           singleItem:true
         });
       });
+      
+      $(".js-mobile-menu-button").on('click', function() {
+        if ($(".navigation-menu").height() == 0 || $(".navigation-menu").height() == "0px") {
+          let heightMenu = $(".navigation-menu>li").length * 40;
+          $(".navigation-menu").css("height", heightMenu + "px");
+        }
+        else {
+          $(".navigation-menu").css("height", 0);
+        }
+      });
+      jQuery(document).ready(function() {
+        jQuery(".owl-carousel").owlCarousel({
+          autoPlay: true,
+          slideSpeed : 300,
+          singleItem:true
+        });
+      });
     }
   };
 })(jQuery, Drupal);
-
-jQuery(document).ready(function() {
-    
-    jQuery(".owl-carousel").owlCarousel({
-	    autoPlay: true,
-	    slideSpeed : 300,
-      singleItem:true
-	  });
-   
-  });
